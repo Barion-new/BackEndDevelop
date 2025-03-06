3a818e78d760ebc4fba7ae07ec627127a1e574a4
22-JWL <jck1004ee@gmail.com>
2025-03-07 03:06:33 +0900

    init

---
+++ src/main/java/me/barion/capstoneprojectbarion/CapstoneProjectBarionApplication.java
@@ -0,0 +1,13 @@
+package me.barion.capstoneprojectbarion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectBarionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneProjectBarionApplication.class, args);
+    }
+
+}
