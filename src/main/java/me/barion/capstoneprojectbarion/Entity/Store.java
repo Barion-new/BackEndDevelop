d3cba7c7c9cb2894f174d74fd7390d35d1ad6112
koyy418 <younko418@gmail.com>
2025-03-16 16:34:26 +0900

    Entity 패키지 추가, Store 구현

---
+++ src/main/java/me/barion/capstoneprojectbarion/Entity/Store.java
@@ -0,0 +1,49 @@
+package me.barion.capstoneprojectbarion.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "store")
+public class Store {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "store_id")
+    private Integer storeId;
+
+    @Column(name = "store_name", nullable = false, length = 100)
+    private String storeName;
+
+    @Column(name = "store_type", length = 100)
+    private String storeType;
+
+    @Column(name = "password", length = 100)
+    private String password;
+
+    @Column(name = "onboarding_status")
+    private Boolean onboardingStatus;
+
+    public Store() {}
+
+    public Store(String storeName, String storeType, String password, Boolean onboardingStatus) {
+        this.storeName = storeName;
+        this.storeType = storeType;
+        this.password = password;
+        this.onboardingStatus = onboardingStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Store{" +
+                "storeId=" + storeId +
+                ", storeName='" + storeName + '\'' +
+                ", storeType='" + storeType + '\'' +
+                ", password='" + password + '\'' +
+                ", onboardingStatus=" + onboardingStatus +
+                '}';
+    }
+}
