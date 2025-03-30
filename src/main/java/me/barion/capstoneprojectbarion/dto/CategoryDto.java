133092c4e01c565cd193f43cc0a2bfd5f930fbdc
koyy418 <younko418@gmail.com>
2025-03-30 16:33:37 +0900

    직원 정보 추가, 삭제, 조회 기능

--- src/main/java/me/barion/capstoneprojectbarion/dto/CategoryDto.java
+++ src/main/java/me/barion/capstoneprojectbarion/dto/CategoryDto.java
@@ -3,18 +3,11 @@ package me.barion.capstoneprojectbarion.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
 @Setter
+@Getter
 public class CategoryDto {
 
     private Integer categoryId;
     private String categoryName;
 
-    public void setCategoryId(Integer categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
 }
