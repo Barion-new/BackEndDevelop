b56c07c3b64857ca432deb6476055f26fa16c6e3
koyy418 <younko418@gmail.com>
2025-04-13 04:09:18 +0900

    오류 수정

--- src/main/java/me/barion/capstoneprojectbarion/Entity/Sales.java
+++ src/main/java/me/barion/capstoneprojectbarion/Entity/Sales.java
@@ -2,9 +2,14 @@
 package me.barion.capstoneprojectbarion.Entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "Sales")
 public class Sales {
 
